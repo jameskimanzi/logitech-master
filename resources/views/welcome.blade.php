9716f5e510a35c326484635548cd93b61fe8b25a
mwikyah <jamespauloh@gmail.com>
2019-08-22 10:03:23 +0300

    changes

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ==== Document Title ==== -->
+    <title>Logitech Consultants Ltd.</title>
+
+    <!-- ==== Document Meta ==== -->
+    <meta name="author" content="Logitech Consultants">
+    <meta name="description" content="Logitech Consultants , Logitech">
+    <meta name="keywords" content="IT consultancy, Logitech Consultants Ltd">
+
+    <!-- ==== Favicon ==== -->
+    <link rel="icon" href="favicon.png" type="image/png">
+
+    <!-- ==== Google Font ==== -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700%7CRoboto:300,400,400i,500,700">
+
+    <!-- ==== Plugins Bundle ==== -->
+    <link rel="stylesheet" href="{{asset('css/plugins.min.css')}}">
+
+    <!-- ==== Main Stylesheet ==== -->
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+
+    <!-- ==== Responsive Stylesheet ==== -->
+    <link rel="stylesheet" href="{{asset('css/responsive-style.css')}}">
+
+    <!-- ==== Color Scheme Stylesheet ==== -->
+    <link rel="stylesheet" href="{{asset('css/colors/color-1.css')}}">
+
+    <!-- ==== Custom Stylesheet ==== -->
+    <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+
+    <!-- ==== HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries ==== -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<!-- Preloader Start -->
+<div id="preloader">
+    <div class="preloader--inner"></div>
+</div>
+<!-- Preloader End -->
+
+<!-- Wrapper Start -->
+<div class="wrapper">
+    <!-- Coming Soon Section Start -->
+    <div class="coming-soon--section" data-bg-img="img/coming-soon-img/bg.jpg">
+        <div class="cs--bg hidden-xs">
+            <div class="cs--bg-inner" data-bg-img="img/light-bg.png"></div>
+        </div>
+
+        <div class="vc--parent">
+            <div class="vc--child">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <!-- 404 Content Start -->
+                            <div class="cs--content text-white text-center pt--80 pb--80">
+                                <div class="caption fs--18 fw--700 text-uppercase">
+                                    <p>We are <strong>Coming Soon</strong></p>
+                                </div>
+
+                                <!-- Countdown Start -->
+                                <div class="countdown" data-countdown="2019/02/05"></div>
+                                <!-- Countdown End -->
+
+                                <div class="desc fs--18 fw--500">
+                                    <p>Be the first to know about the opening</p>
+                                </div>
+
+                                <!-- Newsletter Widget Start -->
+                                <div class="newsletter--widget style--1" data-form="validate">
+                                    <form
+                                        action="https://themelooks.us13.list-manage.com/subscribe/post?u=79f0b132ec25ee223bb41835f&amp;id=f4e0e93d1d"
+                                        method="post" name="mc-embedded-subscribe-form" target="_blank">
+                                        <div class="input-group">
+                                            <input type="email" name="EMAIL" placeholder="Enter your emil address"
+                                                   class="form-control" autocomplete="off" required>
+
+                                            <div class="input-group-btn">
+                                                <button type="submit" class="btn-link"><i class="fa fa-send-o"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <!-- Newsletter Widget End -->
+
+                                <div class="social pt--40">
+                                    <ul class="nav">
+                                        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                                        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                                        <li><a href="#"><span class="fa fa-instagram"></span></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- 404 Content End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Coming Soon Section End -->
+</div>
+<!-- Wrapper End -->
+
+<!-- ==== Plugins Bundle ==== -->
+<script src="{{asset('js/plugins.min.js')}}"></script>
+
+<!-- ==== Main Script ==== -->
+<script src="{{asset('js/main.js')}}"></script>
+
+</body>
+
+</html>
